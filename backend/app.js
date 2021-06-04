2169a5f88d27dc1eb2ee1bddc3b39c7592e025e9
Achille Deribreux <achille@deribreux.be>
2021-06-04 10:13:26 +0200

    Init

---
+++ backend/app.js
@@ -0,0 +1,8 @@
+const express = require('express');//Importe express
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose'); 
+
+
+
+
+module.exports = app;//exporte l'application pour pourvoir l'utiliser depuis les autres fichiers tels que le serveur node
