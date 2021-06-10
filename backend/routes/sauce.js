c00b3c9212a2773db5e4b61ecf806d57b1a003c7
Achille Deribreux <achille@deribreux.be>
2021-06-10 12:01:56 +0200

    Unique validator

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('../middleware/multer-config');
+const auth = require('../middleware/auth');
 const sauceCtrl = require('../controllers/sauce');
 const sauce = require('../models/sauce');
 
-router.post('/:id/like', sauceCtrl.like);
+router.post('/:id/like', auth, sauceCtrl.like);
 
-router.post('/', multer, sauceCtrl.createSauce);
+router.post('/', auth, multer, sauceCtrl.createSauce);
 
-router.get('/',sauceCtrl.displaySauces);
+router.get('/', auth, sauceCtrl.displaySauces);
 
-router.get('/:id', sauceCtrl.displaySauce);
+router.get('/:id', auth, sauceCtrl.displaySauce);
 
-router.put('/:id',multer,sauceCtrl.modifySauce);
+router.put('/:id',auth, multer,sauceCtrl.modifySauce);
 
-router.delete('/:id', sauceCtrl.deleteSauce);
+router.delete('/:id',auth,  sauceCtrl.deleteSauce);
 
 module.exports = router;
